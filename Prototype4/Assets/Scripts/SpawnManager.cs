b66ccd9237157d1a883f6bbc12ccba7c04a0b944
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-04-10 21:05:23 -0500

    Prototype4

---
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public Text waveText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup(1);
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i=0;i < enemiesToSpawn;i++)
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    void SpawnPowerup(int powerupsToSpawn)
+    {
+        for (int i = 0; i < powerupsToSpawn; i++)
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waveText.text = "Wave: " + waveNumber;
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if (enemyCount == 0)
+        {
+            if (waveNumber >= 10)
+            {
+                GameObject.FindGameObjectWithTag("WinManager").GetComponent<WinManager>().win = true;
+                GameObject.FindGameObjectWithTag("WinManager").GetComponent<WinManager>().gameOver = true;
+            }
+            else
+            {
+                waveNumber++;
+                SpawnEnemyWave(waveNumber);
+                if(GameObject.FindGameObjectsWithTag("Powerup").Length<=0)
+                    SpawnPowerup(1);
+            }
+        }
+    }
+}
---
+++ Prototype4/Assets/Scripts/WinManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinManager : MonoBehaviour
+{
+    public bool gameOver = false;
+    public bool win = false;
+    public GameObject gameOverText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            gameOverText.SetActive(true);
+            Time.timeScale = 0;
+            if(win)
+            {
+                gameOverText.GetComponent<Text>().text = "You Win!\nPress R to Restart!";
+            }
+            else
+            {
+                gameOverText.GetComponent<Text>().text = "You Lose!\nPress R to Restart!";
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
