8613f3fa9fb887d0d55552811a2d3d0cfb39bbf2
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-04-10 22:34:21 -0500

    Challenge4

---
+++ Challenge4/Assets/Challenge 4/Scripts/GameOverManager.cs
@@ -0,0 +1,41 @@
+﻿/*
+* Gerard Lamoureux
+* Assignment 7
+* Handles Game Over and Win Text/Restart
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public bool gameOver = false;
+    public bool win = false;
+    public GameObject gameOverText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            gameOverText.SetActive(true);
+            Time.timeScale = 0;
+            if (win)
+            {
+                gameOverText.GetComponent<Text>().text = "You Win!\nPress R to Restart!";
+            }
+            else
+            {
+                gameOverText.GetComponent<Text>().text = "You Lose!\nPress R to Restart!";
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Challenge4/Assets/Challenge 4/Scripts/StartManager.cs
@@ -0,0 +1,28 @@
+﻿/*
+* Gerard Lamoureux
+* Assignment 7
+* Controls Starting Text
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            Destroy(gameObject);
+        }
+    }
+}
